ded4b4888e9e5c2d14a04d9e9ab445580c1d8831
fenglin <huaiyinnianshao28@163.com>
2018-09-21 15:27:34 +0800

    启动服务消费者feign中的断路器hystrix

---
+++ eureka-consumer-feign/src/main/java/com/example/demo/feignInterface/ClientHystrix.java
@@ -0,0 +1,15 @@
+package com.example.demo.feignInterface;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by lenovo on  九月
+ */
+
+@Component
+public class ClientHystrix implements  DcClient {
+    @Override
+    public String consumer() {
+        return "hystrix";
+    }
+}
--- eureka-consumer-feign/src/main/java/com/example/demo/feignInterface/DcClient.java
+++ eureka-consumer-feign/src/main/java/com/example/demo/feignInterface/DcClient.java
@@ -8,12 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 
 /**
- * 使用@FeignClient注解来指定这个接口所要调用的服务名称，
+ * 使用@FeignClient注解。其中的value指定这个接口所要调用的服务名称，fallback指定hystrix的回调类
  * 接口中定义的各个函数使用Spring MVC的注解就可以来绑定服务提供方的REST接口
  */
-@FeignClient("eureka-client")
+@FeignClient(value = "eureka-client",fallback = ClientHystrix.class)
 public interface DcClient {
-
-     @GetMapping("/dc")
+     @GetMapping(value = "/dc")
      String consumer();
 }
