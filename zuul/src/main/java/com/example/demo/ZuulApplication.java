b63cd5f5e1e9203248ef1bfcc8058ef5a11056ed
fenglin <475534279@qq.com>
2018-09-22 23:00:50 +0800

    通过zuul进行服务过滤

--- zuul/src/main/java/com/example/demo/ZuulApplication.java
+++ zuul/src/main/java/com/example/demo/ZuulApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import com.example.demo.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
 public class ZuulApplication {
+
+    @Bean
+    public AccessFilter  accessFilter() {
+        return new AccessFilter();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
