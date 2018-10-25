b73a744a32200ab994011701f2114b557b3cf311
fenglin <huaiyinnianshao28@163.com>
2018-10-25 11:31:08 +0800

    高可用注册中心eureka-server集群

---
+++ eureka-server-cluster/src/main/java/com/example/demo/EurekaServerApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    /**
+     * 高可用注册中心eureka-server集群，需要通过命令行加载properties文件启动。
+     * java -jar  eureka-server-0.0.1-SNAPSHOT.jar  --spring.profiles.active=peer1
+     * java -jar  eureka-server-0.0.1-SNAPSHOT.jar  --spring.profiles.active=peer2
+     *
+     * 启动方式 ，详情见 http://blog.didispace.com/springcloud6/
+     * @param args
+     */
+    public static void main(String[] args) {
+       new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
+    }
+}
