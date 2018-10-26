f9b80049fe8a2e9d37983bec941f2bfcc6f02484
fenglin <huaiyinnianshao28@163.com>
2018-10-26 17:33:53 +0800

    HystrixDash进行监控，展示数据

---
+++ hystrix-dashboard/src/main/java/com/example/demo/HystrixDashboard.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Hystrix Dashboard进行监控。
+ * 启动应用，访问 http://localhost:1301/hystrix  可以看到监控页面。
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboard {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboard.class, args);
+    }
+}
