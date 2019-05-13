6781face353b6f160ee7b9cfbfe0d75a8dd8cb06
lin_gr <475534279@qq.com>
2019-05-13 16:16:40 +0800

    补充ZuulFilter的注释

--- zuul/src/main/java/com/example/demo/filter/AccessFilter.java
+++ zuul/src/main/java/com/example/demo/filter/AccessFilter.java
@@ -11,24 +11,37 @@ import javax.servlet.http.HttpServletRequest;
 public class AccessFilter extends ZuulFilter {
     private static Logger log=Logger.getLogger(AccessFilter.class);
 
+    /**
+     * 过滤的类型，可以返回"pre"、"route"、"post"、"error"
+     * filterType() - 按类型对过滤器进行分类。 可以返回用于预路由过滤的“pre”，用于路由到原点的“route”，用于后路由过滤的“post”，用于错误处理的“error”。
+     * @return
+     */
     @Override
     public String filterType() {
         return "pre";
     }
 
+    /**
+     * 返回数字，表示过滤器执行的顺序。filterOrders返回的结果不需要是连续的顺序，如1，2，3，4。
+     * @return
+     */
     @Override
     public int filterOrder() {
         return 0;
     }
 
-
+    /**
+     * 是否过滤
+     * @return
+     */
     @Override
     public boolean shouldFilter() {
         return true;
     }
 
     /**
-     * 设置过滤。url只有带有有效参数才可以访问。
+     * 过滤器的具体逻辑。
+     * 以下方法表示设置过滤。url只有带有有效参数才可以访问。
      * @return
      */
     @Override
